c06fe54eb763cc9037a9a6db3da2d18a8c113fe2
Nicolas Martins <nicolas.edm7@gmail.com>
2026-02-23 22:01:39 -0300

    feat(job): add job DTOs, use cases and REST endpoints

--- src/main/java/dev/nicolas/JobTracker/infrastructure/persistence/jpa/repository/JobJpaRepository.java
+++ src/main/java/dev/nicolas/JobTracker/infrastructure/persistence/jpa/repository/JobJpaRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface JobRepository extends JpaRepository<JobJpaEntity, UUID> {
+public interface JobJpaRepository extends JpaRepository<JobJpaEntity, UUID> {
 }
