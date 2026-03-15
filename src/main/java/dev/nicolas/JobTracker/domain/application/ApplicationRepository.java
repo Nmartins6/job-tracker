e7ae9fb3937e717f78383492eba590aaa9e6491b
Nicolas Martins <nicolas.edm7@gmail.com>
2026-03-15 20:48:43 -0300

    feat(application): introduce Application aggregate and macro status

---
+++ src/main/java/dev/nicolas/JobTracker/domain/application/ApplicationRepository.java
@@ -0,0 +1,14 @@
+package dev.nicolas.JobTracker.domain.application;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface ApplicationRepository {
+
+    Application save(Application application);
+
+    Optional<Application> findById(UUID id);
+
+    List<Application> findAll();
+}
